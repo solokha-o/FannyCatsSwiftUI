bed02525dcb99dd33ca597c517f95cbfbdb519f5
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-09-29 15:13:52 +0300

    Initial Commit

---
+++ FannyCatsSwiftUI/FannyCatsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FannyCatsSwiftUIApp.swift
+//  FannyCatsSwiftUI
+//
+//  Created by Oleksandr Solokha on 29.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct FannyCatsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
