5cda78fabf0ad090fd913f30cdd95b044fe7b57f
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-10-19 17:58:57 +0300

    in Assets add large image for tab item in TabMenu state for to animate item's image from small to large and animate tab image
    create GuessCat

---
+++ FannyCatsSwiftUI/GuessCat/GuessCat.swift
@@ -0,0 +1,20 @@
+//
+//  GuessCat.swift
+//  FannyCatsSwiftUI
+//
+//  Created by Oleksandr Solokha on 19.10.2020.
+//
+
+import SwiftUI
+
+struct GuessCat: View {
+    var body: some View {
+        Text("Guess Cat menu")
+    }
+}
+
+struct GuessCat_Previews: PreviewProvider {
+    static var previews: some View {
+        GuessCat()
+    }
+}
--- FannyCatsSwiftUI/TabMenu.swift
+++ FannyCatsSwiftUI/TabMenu.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import UIKit
 
 struct TabMenu: View {
+    // state for to animate item's image from small to large
+    @State var tabItem1 = false
+    @State var tabItem2 = true
     
     init() {
         UITabBar.appearance().barTintColor = .systemTeal
@@ -19,10 +22,32 @@ struct TabMenu: View {
         TabView {
             //configure 1-st item "Cat Breeds"
             BreedsList()
+                .onAppear {
+                    self.tabItem1.toggle()
+                    self.tabItem2.toggle()
+                }
                 .tabItem {
-                    Image("item1")
-                    Text("Cat breeds")
-                        .foregroundColor(Color.black)
+                    VStack {
+                        Image(self.tabItem1 ? "item1-1" : "item1")
+                        Text("Cat breeds")
+                            .foregroundColor(Color.black)
+                    }
+                    .animation(.interpolatingSpring(mass: 0.7, stiffness: 200, damping: 10, initialVelocity: 4))
+                    
+                }
+        //Configure 2-nd item "Guess Cat"
+            GuessCat()
+                .onAppear {
+                    self.tabItem1.toggle()
+                    self.tabItem2.toggle()
+                }
+                .tabItem {
+                    VStack {
+                        Image(self.tabItem2 ? "item2-1" : "item2")
+                        Text("Guess cat")
+                            .foregroundColor(Color.black)
+                    }
+                    .animation(.interpolatingSpring(mass: 0.7, stiffness: 200, damping: 10, initialVelocity: 4))
                 }
         }
         .accentColor(Color.black)
@@ -32,5 +57,27 @@ struct TabMenu: View {
 struct TabMenu_Previews: PreviewProvider {
     static var previews: some View {
         TabMenu()
+    }
+}
+
+// Animating font size
+struct AnimatableSFImage: AnimatableModifier {
+    var size: CGFloat
+    
+    var animatableData: CGFloat {
+        get { size }
+        set { size = newValue }
+    }
+    
+    func body(content: Self.Content) -> some View {
+        content
+            .font(.system(size: size))
+    }
+}
+
+// helper extension
+extension Image {
+    func animatingSF(size: CGFloat) -> some View {
+        self.modifier(AnimatableSFImage(size: size))
     }
 }
