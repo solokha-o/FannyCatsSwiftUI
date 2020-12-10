f824c78c1932075d6962e64a998e17d17d3c4767
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-12-10 11:14:35 +0200

    create ShareSheet

--- FannyCatsSwiftUI/CatGallery/CatImageDetail.swift
+++ FannyCatsSwiftUI/CatGallery/CatImageDetail.swift
@@ -51,27 +51,7 @@ struct CatImageDetail: View {
         }
     }
 }
-//configure ShareSheet like UIActivityViewController
-struct ShareSheet: UIViewControllerRepresentable {
-    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
-    
-    let activityItems: [Any]
-    let applicationActivities: [UIActivity]? = nil
-    let excludedActivityTypes: [UIActivity.ActivityType]? = nil
-    let callback: Callback? = nil
-    
-    func makeUIViewController(context: Context) -> UIActivityViewController {
-        let controller = UIActivityViewController(
-            activityItems: activityItems,
-            applicationActivities: applicationActivities)
-        controller.excludedActivityTypes = excludedActivityTypes
-        controller.completionWithItemsHandler = callback
-        return controller
-    }
-    
-    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
-    }
-}
+
 
 struct CatImageDetail_Previews: PreviewProvider {
     static var previews: some View {
